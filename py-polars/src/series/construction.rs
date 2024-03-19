404ac7ad33fca69d1cbfc92a360237b3ef7ad056
Stijn de Gooijer <stijndegooijer@gmail.com>
2024-03-19 09:23:52 +0100

    Fix bools

--- py-polars/src/series/construction.rs
+++ py-polars/src/series/construction.rs
@@ -91,7 +91,7 @@ impl PySeries {
 #[pymethods]
 impl PySeries {
     #[staticmethod]
-    fn new_opt_bool(name: &str, values: &PyAny, strict: bool) -> PyResult<Self> {
+    fn new_opt_bool(name: &str, values: &PyAny, _strict: bool) -> PyResult<Self> {
         let len = values.len()?;
         let mut builder = BooleanChunkedBuilder::new(name, len);
 
@@ -100,15 +100,8 @@ impl PySeries {
             if value.is_none() {
                 builder.append_null()
             } else {
-                match value.extract::<bool>() {
-                    Ok(v) => builder.append_value(v),
-                    Err(e) => {
-                        if strict {
-                            return Err(e);
-                        }
-                        builder.append_null()
-                    },
-                }
+                let v = item.extract::<bool>()?;
+                builder.append_value(v)
             }
         }
         let ca = builder.finish();
